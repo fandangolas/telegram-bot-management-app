54319d21e0bd0199e1a1391413b519554505ef88
Nícolas B <fandangolas@localhost.localdomain>
2020-05-20 02:18:06 -0300

    refatorando e uso do novo msgHandler/dados fakes

--- src/index.js
+++ src/index.js
@@ -1,64 +1,20 @@
 const TelegramBot = require('node-telegram-bot-api');
+var _ = require('lodash');
+require('dotenv/config');
 
-// replace the value below with the Telegram token you receive from @BotFather
-const token = '1271816008:AAHYO3dYcXGRDPwlKtJ4XjEEqVKQlKyucJQ';
-const safado = "CAACAgEAAxkBAAIBD16_VJIPb9v_waPHfw7IW2fCCI7yAALdAwACS1KPEoLhG4PEFyvLGQQ";
-const bobo = "CAACAgEAAxkBAAIBDl6_VFnJNnBTNlDD7W2nNu30eg9zAAIzAAN09jML9gLmBxF7wQUZBA";
+import telegramMessageHandler from "./services/telegramMessageHandler";
+import stickersWithRules from "./stickersWithRules";
+import messagesWithRules from "./messagesWithRules";
 
-// Create a bot that uses 'polling' to fetch new updates
+const token = process.env.BOT_TOKEN;
+
 const bot = new TelegramBot(token, { polling: true });
+const messageHandler = telegramMessageHandler(bot);
 
-// Matches "/echo [whatever]"
-bot.onText(/[BbCcDdFfGgHhJjLlMmNnPpQqRrSsTtVvWwXxYyZz]{4,}/g, (msg, match) => {
-    // 'msg' is the received Message from Telegram
-    // 'match' is the result of executing the regexp above on the text content
-    // of the message
+_.mapValues(stickersWithRules, sWr =>
+    messageHandler.sendStickerUsingRegexRule(sWr.rule, sWr.stickerId)
+);
 
-    const chatId = msg.chat.id;
-    console.log(match);
-    
-    bot.sendSticker(chatId, bobo);
-
-    // send back the matched "whatever" to the chat
-    //bot.sendMessage(chatId, resp);
-});
-
-bot.onText(/^.*\b(morena|MORENA)\b.*$/, (msg, match) => {
-    // 'msg' is the received Message from Telegram
-    // 'match' is the result of executing the regexp above on the text content
-    // of the message
-
-    const chatId = msg.chat.id;
-    bot.sendSticker(chatId, safado);
-
-    // send back the matched "whatever" to the chat
-    //bot.sendMessage(chatId, resp);
-});
-
-bot.onText(/^.*\b(mamou)\b.*$/, (msg, match) => {
-    // 'msg' is the received Message from Telegram
-    // 'match' is the result of executing the regexp above on the text content
-    // of the message
-
-    const chatId = msg.chat.id;
-    const resp = `mamaste???`; // the captured "whatever"
-
-    // send back the matched "whatever" to the chat
-    bot.sendMessage(chatId, resp);
-});
-
-bot.onText(/^.*\b(MAMOU)\b.*$/, (msg, match) => {
-    // 'msg' is the received Message from Telegram
-    // 'match' is the result of executing the regexp above on the text content
-    // of the message
-
-    const chatId = msg.chat.id;
-    const resp = `MAMASTE???`; // the captured "whatever"
-
-    // send back the matched "whatever" to the chat
-    bot.sendMessage(chatId, resp);
-});
-
-bot.on("sticker", msg => {
-    console.log(msg);
-});
+_.mapValues(messagesWithRules, mWr =>
+    messageHandler.sendMessageUsingRegexRule(mWr.rule, mWr.message)
+);
