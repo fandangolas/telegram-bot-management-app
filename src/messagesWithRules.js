97b7b1314a79d03ef7098314d900d82f8f71b14b
Nícolas B <fandangolas@localhost.localdomain>
2020-05-20 02:16:21 -0300

    criando dados fakes q serão substituidos no futuro

---
+++ src/messagesWithRules.js
@@ -0,0 +1,13 @@
+const messagesWithRules = {
+  mamou1: {
+    rule: /^.*\b(mamou)\b.*$/,
+    message: 'mamaste???'
+  },
+  
+  mamou2: {
+    rule: /^.*\b(MAMOU)\b.*$/,
+    message: 'MAMASTE???'
+  }
+};
+
+export default messagesWithRules;
---
+++ src/stickersWithRules.js
@@ -0,0 +1,13 @@
+const stickersWithRules = {
+  bobo: {
+    rule: /[BbCcDdFfGgHhJjLlMmNnPpQqRrSsTtVvWwXxYyZz]{4,}/g,
+    stickerId: 'CAACAgEAAxkBAAIBDl6_VFnJNnBTNlDD7W2nNu30eg9zAAIzAAN09jML9gLmBxF7wQUZBA'
+  },
+  
+  safado: {
+    rule: /^.*\b(morena|MORENA)\b.*$/,
+    stickerId: 'CAACAgEAAxkBAAIBD16_VJIPb9v_waPHfw7IW2fCCI7yAALdAwACS1KPEoLhG4PEFyvLGQQ'
+  }
+};
+
+export default stickersWithRules;
