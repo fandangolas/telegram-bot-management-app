fffe57d4948295f7d2121f0c8a82870a47fa7094
Nícolas B <fandangolas@localhost.localdomain>
2020-05-20 02:17:04 -0300

    adicionando telegramMessageHandler

---
+++ src/services/telegramMessageHandler.js
@@ -0,0 +1,25 @@
+const telegramMessageHandler = (bot) => {
+
+  const sendStickerUsingRegexRule = (rule, sticker) => {
+    bot.onText(rule, msg => {
+      const chatId = msg.chat.id;
+
+      bot.sendSticker(chatId, sticker);
+    });
+  };
+
+  const sendMessageUsingRegexRule = (rule, message) => {
+    bot.onText(rule, msg => {
+      const chatId = msg.chat.id;
+
+      bot.sendMessage(chatId, message);
+  });
+  };
+
+  return {
+    sendStickerUsingRegexRule,
+    sendMessageUsingRegexRule
+  };
+};
+
+export default telegramMessageHandler;
