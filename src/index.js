85ac7fc6a71fd3acc589e3e7389f7963ab4e4fd7
Nícolas B <fandangolas@localhost.localdomain>
2020-05-20 02:32:22 -0300

    refatorando require -> import

--- src/index.js
+++ src/index.js
@@ -1,14 +1,15 @@
-const TelegramBot = require('node-telegram-bot-api');
-var _ = require('lodash');
-require('dotenv/config');
-
-import telegramMessageHandler from "./services/telegramMessageHandler";
-import stickersWithRules from "./stickersWithRules";
+import dotenv from "dotenv";
 import messagesWithRules from "./messagesWithRules";
+import telegramMessageHandler from "./services/telegramMessageHandler";
+import TelegramBot from "node-telegram-bot-api";
+import stickersWithRules from "./stickersWithRules";
+import _ from "lodash";
+
+dotenv.config();
 
 const token = process.env.BOT_TOKEN;
-
 const bot = new TelegramBot(token, { polling: true });
+
 const messageHandler = telegramMessageHandler(bot);
 
 _.mapValues(stickersWithRules, sWr =>
